d45c86a26e9079cf91c4c33bb149ae3cc95d9bee
Milton <miltonsoftwareengineer@gmail.com>
2022-09-03 15:49:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0 ; i < array.length ; i ++ ){
+    let valueToAdd = target - array[i];
+
+    for(let x = i + 1 ; x < array.length; x ++ ){
+      if(array[x] === valueToAdd) return true; 
+    } 
+  }
+  return false ; 
 }
 
 /* 
@@ -9,7 +17,11 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
-
+// loop through each element in array
+// get the number that we should add to element to reach target
+// create an inner loop that compares to each element in the array
+// if any of them add up return true
+// if we reach end of array return false  
 /*
   Add written explanation of your solution here
 */
